6fd37a3af3ea79232fd44eb1e6a5618ce510b9c4
mturkoglu23 <54291142+mturkoglu23@users.noreply.github.com>
2021-10-31 00:09:51 +0300

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,6 @@
-clc;
-clear;
+clc;clear;
 
-load fea_new2
+load data
 
 feats=xxx(:,1:89);
 label=xxx(:,90);
@@ -13,30 +12,13 @@ Feats=Feats';
 labels=double(label);
 
 %%
-accuracy=0;
-while accuracy<0.96
 data = Feats ;  % your data   
 labels1=categorical(label);
-% [m,n] = size(data) ; 
-% 
-% [trainInd,valInd,testInd] = dividerand(4200,0.8,0.1,0.1);
-% % load trainInd
-% % load valInd
-% % load testInd
-% Feats_train = data(trainInd,:) ;  
-% Feats_test = data(testInd,:) ;
-% Feats_val = data(valInd,:) ;
-% % 
-% labels_train = labels1(trainInd,:) ;  
-% labels_test = labels1(testInd,:) ;
-% labels_val = labels1(valInd,:) ;
 
-%%
-
-% cv  = cvpartition(size(data,1), "HoldOut", 0.2);
-% 
-% train_split=cv.training;
-% test_split=cv.test;
+%% train-test splitting
+%cv  = cvpartition(size(data,1), "HoldOut", 0.2);
+%train_split=cv.training;
+%test_split=cv.test;
 load train_split
 load test_split
 Feats_train = data(train_split, :);
@@ -45,7 +27,7 @@ Feats_test = data(test_split, :);
 labels_test = labels1(test_split, :);
 
 
-%%
+%% Model Setup
 inputSize = 1;
 numClasses = 4;
 lgraph = layerGraph();
@@ -77,17 +59,50 @@ lgraph = connectLayers(lgraph,"sequence","lstm");
 lgraph = connectLayers(lgraph,"dropout_1","addition/in1");
 lgraph = connectLayers(lgraph,"dropout_2","addition/in2");
 
+%% Parameter setting
  options = trainingOptions('adam', ...
-    'MaxEpochs',30, ...
+    'MaxEpochs',50, ...
     'MiniBatchSize', 16, ...
     'InitialLearnRate', 0.0001, ...
     'SequenceLength', 89, ...
      'Verbose',false);
-%       'Plots','training-progress',... 
 
-%      'ExecutionEnvironment','multi-gpu', ...
+%% Training of the proposed parallel based RNNs model 
+net = trainNetwork(Feats_train,labels_train,lgraph,options);
 
-net = trainNetwork(Feats,labels1,lgraph,options);
-[YPred_rnn,scores] = classify(net,Feats_test);
-accuracy = mean(YPred_rnn == labels_test)
 
+%% The second stage of the proposed model
+% Feature Extraction
+for i=1:length(Feats_train)
+value=Feats_train{i};
+layer='addition';
+feat_train(:,i) = activations(net,value,layer);
+end
+
+for i=1:length(Feats_test)
+value=Feats_test{i};
+layer='addition';
+feat_test(:,i) = activations(net,value,layer);
+end
+
+% Classification with SVM method
+featuresTrain=feat_train';
+YTrain=double(labels_train);
+featuresTest=feat_test';
+YTest=double(labels_test);
+
+template = templateSVM(...
+    'KernelFunction', 'polynomial', ...
+    'PolynomialOrder',3, ...
+    'KernelScale', 'auto', ...
+    'BoxConstraint', 1, ...
+    'Standardize', true);
+    
+classifier = fitcecoc(featuresTrain,YTrain,...
+    'Learners', template,...
+    'Coding', 'onevsall');
+
+% Testing of proposed parallel RNNs based SVM model
+YPred = predict(classifier,featuresTest);
+accuracy = mean(YPred == YTest);
+fprintf('Result: %d', accuracy)
